90a15b03f87047e19070f5dc83b374de542887ef
Moutigll <moutig-tan@proton.me>
2023-10-11 19:53:17 +0200

    Update untilted.py

--- untilted.py
+++ untilted.py
@@ -1,12 +1,30 @@
 from kandinsky import *
 from time import *
 from ion import *
-plyra=[50,101,(0,0,255),33,134]
-plyrb=[250,101,(255,0,0),166,267]
-bg=(255,255,255)
-frames=0
-inputs=["4",1,-1,"0",1,1,"1",0,-1,"2",0,1,"/",1,-1,"EXE",1,1,"+",0,-1,"-",0,1]
-bl=[False,0,False,0,0,False,0,0,False,0,0,False,0,False,0,0,False,0,0,False,0,0]
+def start():
+  global plyra,plyrb,bg,frames,inputs,bl,bgc,intrf,intrfc,health
+  plyra=[50,101,(0,0,255),33,134]
+  plyrb=[250,101,(255,0,0),166,267]
+  health=[6,6]
+  bg=(255,255,255)
+  intrf=(255,0,255)
+  frames=0
+  inputs=["4",1,-1,"0",1,1,"1",0,-1,"2",0,1,"/",1,-1,"EXE",1,1,"+",0,-1,"-",0,1]
+  bl=[False,0,False,0,0,False,0,0,False,0,0,False,0,False,0,0,False,0,0,False,0,0]
+  fill_rect(0,0,320,222,bg)
+  bgc=get_pixel(1,1)
+  draw_plyr(plyrb[0],plyrb[1],plyrb[2])
+  draw_plyr(plyra[0],plyra[1],plyra[2])
+  fill_rect(157,0,6,222,intrf)
+  intrfc=get_pixel(158,1)
+  fill_rect(29,0,1,222,intrf)
+  fill_rect(290,0,1,222,intrf)
+  draw_bla(14,17,True)
+  draw_bla(306,199,False)
+  fill_rect(4,3,7,33,intrf)
+  fill_rect(310,186,7,33,intrf)
+  draw_health(True,health[0])
+  draw_health(False,health[1])
 def draw_plyr(x,y,c):
   fill_rect(x,y,20,20,c)
 def draw_bla(x,y,d):
@@ -18,15 +36,16 @@ def draw_bla(x,y,d):
     fill_rect(x-11,y,11,5,(0,0,0))
     fill_rect(x-12,y+1,1,3,(0,0,0))
     fill_rect(x-13,y+2,1,1,(0,0,0))
-draw_plyr(plyrb[0],plyrb[1],plyrb[2])
-draw_plyr(plyra[0],plyra[1],plyra[2])
-fill_rect(157,0,6,222,(0,0,0))
-fill_rect(29,0,1,222,(0,0,0))
-fill_rect(290,0,1,222,(0,0,0))
-draw_bla(14,17,True)
-draw_bla(306,199,False)
-fill_rect(4,3,7,33,(0,0,0))
-fill_rect(310,186,7,33,(0,0,0))
+def draw_health(p,n):
+  if not p:
+    x,y,d=10,130,15
+  else:
+    x,y,d=301,82,-15
+  fill_rect(x,y,10,d*10,bg)
+  for i in range(n):
+    fill_rect(x,y,10,10,(250,100,100))
+    y+=d
+start()
 while True:
   fps=monotonic()
   if not bl[0]:
@@ -63,33 +82,42 @@ while True:
     fill_rect(311,187,5,1,(0,255,0))
     if not bl[13]:
       bl[13]=True
-      bl[14]=plyrb[0]-25
+      bl[14]=plyrb[0]-5
       bl[15]=plyrb[1]+8
     elif not bl[16]:
       bl[16]=True
-      bl[17]=plyrb[0]-25
+      bl[17]=plyrb[0]-5
       bl[18]=plyrb[1]+8
     elif not bl[19]:
       bl[19]=True
-      bl[20]=plyrb[0]-25
+      bl[20]=plyrb[0]-5
       bl[21]=plyrb[1]+8
   c=2
-  a=[1,True]
+  a=[1,True,13,0,"a"]
   for i in range(2):
     for i in range(3):
       if bl[c]:
         fill_rect(bl[c+1],bl[c+2],1,5,(255,255,255))
         bl[c+1]+=a[0]
         draw_bla(bl[c+1],bl[c+2],a[1])
+        col=get_pixel(bl[c+1]+a[2],bl[c+2]+2)
         if bl[c+1] > 276 or bl[c+1] < 43:
           bl[c]=False
-          if a[1]:
-            fill_rect(bl[c+1],bl[c+2],13,5,bg)
-          else:
-            fill_rect(bl[c+1]-12,bl[c+2],13,5,bg)
+          fill_rect(bl[c+1]+a[3],bl[c+2],a[2],5,bg)
+        elif col != bgc and col != intrfc and col != (0,0,0):
+          health[a[3]]-=1
+          draw_health(a[1],health[a[3]])
+          fill_rect(bl[c+1]+a[3],bl[c+2],a[2],5,bg)
+          bl[c]=False
+          if health[a[3]] == 0:
+            fill_rect(0,0,320,222,bg)
+            draw_string("END Player "+a[4]+" win !",50,110)
+            while not keydown(KEY_OK):
+              pass
+            start()
       c+=3
     c=13
-    a=[-1,False]
+    a=[-1,False,-14,1,"b"]
   keys=list(get_keys())
   b=0
   for i in range(len(keys)):
