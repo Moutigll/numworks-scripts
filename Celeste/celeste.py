002bcfdfd9223bca92dda2856787caeae89c7f71
Moutig <moutig-tan@proton.me>
2024-02-01 07:11:49 +0100

    Added movement

--- Celeste/celeste.py
+++ Celeste/celeste.py
@@ -7,9 +7,8 @@ fill_rect(0,0,320,222,(35,)*3)
 fill_rect(96,47,128,128,(0,0,20))
 palet=[(255,0,77),(255,204,170),(29,43,83),(0,135,81),(255,241,232),(41,173,255),(95,87,79)]
 wa=[[0,1,3,4],[0,2,3,5]]
-wac=0
-dir=0
-player=[30,30,[0,0,8,6],[7,0,-1,6],False]
+wac=rft=dir=0
+player=[10,99,[0,0,8,6],[7,0,-1,6],False]
 run=True
 def draw_obj(b,x,y,s,r):
   x1,y1,x2,y2=b[0],b[1],b[2],b[3]
@@ -43,56 +42,78 @@ def walkf(b):
   a=0
   if wac == 2:
     a=-1
-  fill_rect(player[0]+95,player[1]+46,10,10,(0,0,125))
-  draw_obj(player[b+2],player[0],player[1]+a,0,0)
-  draw_obj(walk[wa[b][wac]],player[0]+1,player[1]+5+a,0,0)
+  ref_pl()
+  draw_obj(player[b+2],player[0]-2,player[1]+a-2,0,0)
+  draw_obj(walk[wa[b][wac]],player[0]-1,player[1]+3+a,0,0)
   wac+=1
   player[4]=True
-  sleep(0.1)
 
-draw_obj([0,0,8,6],player[0],player[1],0,0)
-draw_obj(walk[wa[0][0]],player[0]+1,player[1]+5,0,0)
+def ntl(l,j):
+  a=[int(x) for x in str(l[j])]
+  if len(a) == 1:
+    a.append(0)
+    a.append(0)
+  elif len(a) == 2:
+    a[0]=int(str(a[0])+str(a[1]))
+    a.remove(a[1])
+    a.append(0)
+    a.append(0)
+  elif len(a) > 3:
+    a[0]=int(str(a[0])+str(a[1]))
+    a.remove(a[1])
+  return a
+
+def ref_pl():
+  global rft,player
+  fill_rect(player[0]+93,player[1]+44,10,8,(0,0,20))
+  if rft == 1:
+    a=ntl(lvl[0][player[1]//8],(player[0]//8)+1)
+    draw_obj(tiles[a[0]-1],((player[0]//8)+1)*8,(player[1]//8)*8,a[1],a[2])
+  if rft == 2:
+    a=ntl(lvl[0][player[1]//8],(player[0]//8)-1)
+    print(a)
+    draw_obj(tiles[a[0]-1],((player[0]//8)-1)*8,(player[1]//8)*8,a[1],a[2])
+  rft=0
+draw_obj([0,0,8,6],player[0]-2,player[1]-2,0,0)
+draw_obj(walk[wa[0][0]],player[0]-1,player[1]+3,0,0)
+t=monotonic()
 for i in range(16):
   l=lvl[0][i]
   for j in range(16):
-    a=[int(x) for x in str(l[j])]
-    if len(a) == 1:
-      a.append(0)
-      a.append(0)
-    elif len(a) == 2:
-      a[0]=int(str(a[0])+str(a[1]))
-      a.remove(a[1])
-      a.append(0)
-      a.append(0)
-    elif len(a) > 3:
-      a[0]=int(str(a[0])+str(a[1]))
-      a.remove(a[1])
+    a=ntl(l,j)
     if a[0] != 0:
       draw_obj(tiles[a[0]-1],j*8,i*8,a[1],a[2])
+print(monotonic()-t)
 while run:
   if keydown(KEY_DOWN):
-    fill_rect(player[0]+95,player[1]+46,10,10,(0,0,125))
-    draw_obj(updown[0+dir],player[0],player[1],0,0)
+    ref_pl()
+    draw_obj(updown[0+dir],player[0]-2,player[1]-2,0,0)
     player[4]=True
-    sleep(0.1)
   elif keydown (KEY_UP):
-    fill_rect(player[0]+95,player[1]+46,10,10,(0,0,125))
-    draw_obj(updown[2+dir],player[0],player[1],0,0)
+    ref_pl()
+    draw_obj(updown[2+dir],player[0]-2,player[1]-2,0,0)
     player[4]=True
-    sleep(0.1)
+  if player[0]%8 >= 2 and lvl[0][player[1]//8][(player[0]//8)+1] != 0:
+    rft=1
+  elif player[0]%8 <= 2 and lvl[0][player[1]//8][(player[0]//8)-1] != 0:
+    rft=2
   if keydown(KEY_RIGHT):
+    if player[0] < 119 and not (player[0]%8 > 2 and lvl[0][player[1]//8][(player[0]//8)+1] != 0):
+      player[0]+=1
+    dir=0
     if not keydown(KEY_DOWN) and not keydown(KEY_UP):
       walkf(0)
-    dir=0
   elif keydown(KEY_LEFT):
+    if player[0] > 0 and not (player[0]%8 < 2 and lvl[0][player[1]//8][(player[0]//8)-1] != 0):
+      player[0]-=1
+    dir=1
     if not keydown(KEY_DOWN) and not keydown(KEY_UP):
       walkf(1)
-    dir=1
-  elif player[4] and not keydown(KEY_DOWN) and not keydown(KEY_UP):
+  elif player[4] and not keydown(KEY_DOWN)and not keydown(KEY_UP) and not keydown(KEY_LEFT) and not keydown(KEY_RIGHT):
     player[4]=False
-    fill_rect(player[0]+95,player[1]+46,10,10,(0,0,125))
-    draw_obj(player[dir+2],player[0],player[1],0,0)
-    draw_obj(walk[wa[0][0]],player[0]+1,player[1]+5,0,0)
+    ref_pl()
+    draw_obj(player[dir+2],player[0]-2,player[1]-2,0,0)
+    draw_obj(walk[wa[0][0]],player[0]-1,player[1]+3,0,0)
   if keydown(KEY_BACK):
     pass
   if keydown(KEY_ZERO):
